0a7c0407ce2f4fb3041e9bfac72aa2fdb29b5988
yam-yanu <you1095@gmail.com>
2014-05-20 13:29:49 +0900

    ログイン状態によるページ遷移の処理が複雑だったため修正

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,8 +1,9 @@
 class LoginController < ApplicationController
 	def show
 		if current_user
-			redirect_to :controller => 'action', action => 'index'
+			redirect_to :controller => 'actions', :action => 'index'
+		else
+			render :action => "show", :layout => false
 		end
-		render :action => "show", :layout => false
 	end
 end
